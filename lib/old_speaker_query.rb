dfb6c69ac4f4a3c450c8e4f3021903947b1945da
Todd Kaufman <toddkaufman@gmail.com>
2013-01-05 07:54:24 -0500

    Scope Refactor
    Enhanced Scope refactor with spec

---
+++ lib/old_speaker_query.rb
@@ -0,0 +1,17 @@
+class OldSpeakerQuery
+
+  attr_reader :relation
+
+  def initialize(relation = Speaker.scoped)
+    @relation = relation.where("birthdate < ?", Date.civil(1980,1,1))
+  end
+
+  def all
+    @relation.all
+  end
+
+  def find_each(&block)
+    @relation.find_each(&block)
+  end
+
+end
---
+++ spec/old_speaker_query_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+require 'old_speaker_query'
+
+describe OldSpeakerQuery do
+  Given!(:jim) { Speaker.create(:name => "Jim Holmes", :birthdate => Date.civil(1945, 5, 12)) }
+  Given!(:jason) { Speaker.create(:name => "Jason Karns", :birthdate => Date.civil(1985, 3, 20)) }
+  Given(:query) { OldSpeakerQuery.new }
+
+  context "Returns Oldies with All" do
+    When(:results) { query.all }
+    Then do
+      results.should have(1).speaker
+      results.first.should == jim
+    end
+  end
+
+  context "Allows Iteration with Each" do
+    Then do
+      query.find_each do |speaker|
+        speaker.name.should == jim.name
+        speaker.birthdate.should == jim.birthdate
+      end
+    end
+  end
+
+  context "Provides Access to the Arel" do
+    When(:result) { query.relation }
+    Then do
+      result.class.should == ActiveRecord::Relation
+    end
+  end
+end
