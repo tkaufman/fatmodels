bfb040bc8957f8f4bd700197d000cf6bfd1f65c6
Jason Karns <jason@karns.name>
2013-01-07 23:04:49 -0500

    Extract Query objects

---
+++ lib/queries/with_comments_query.rb
@@ -0,0 +1,18 @@
+module Queries
+  class WithCommentsQuery
+
+    attr_reader :relation
+
+    def initialize(relation = Feedback.scoped)
+      @relation = relation.where("comment IS NOT NULL")
+    end
+
+    def all
+      @relation.all
+    end
+
+    def find_each(&block)
+      @relation.find_each(&block)
+    end
+  end
+end
